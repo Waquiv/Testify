1d7899b81a9d16a8db1ec448a3584658b1584fda
Waquiv <akhtarwaquiv@gmail>
2023-11-17 10:24:36 +0530

    Experimented with the iframe api(result null)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const iframe= document.querySelector("iframe");
+/*const iframe= document.querySelector("iframe");
 
-const clicky=document.querySelector(".cicky");
+const clicky=document.querySelector(".clicky");
 console.log(clicky)
 
 const arr=['21O0XXPEWPtePt5RMY93Ob', '1EO9dn7V0ufwPHJngZThO5', '5FVd6KXrgO9B3JPmC8OPst', '2DlHlPMa4M17kufBvI2lEN', '4fXGWiVhlOLdhwRDP6pIFG', '3DjYU54tU6YCEy822r8TcY']
@@ -10,3 +10,24 @@ clicky.addEventListener('click', function(){
     //console.log(rand)
     iframe.src=`https://open.spotify.com/embed/track/${arr[rand]}?utm_source=generator`
 })
+Code to display normal spotify embed without controls
+*/
+
+
+//plays the uri track in the "embed-iframe div"
+window.onSpotifyIframeApiReady=(IFrameAPI)=>{
+    console.log('Spotify Iframe API loaded');
+
+    const element = document.getElementById("embed-iframe");
+    console.log(element);
+
+    const options = {
+        uri: 'spotify:track:3vkCueOmm7xQDoJ17W1Pm3'
+    };
+    console.log(options)
+
+    const callback = (EmbedController)=>{};
+
+    IFrameAPI.createController(element, options, callback);
+}
+
