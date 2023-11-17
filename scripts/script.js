e162ad736c558b61e9973cd0a0e5df4acdd1d672
Waquiv <akhtarwaquiv@gmail>
2023-11-17 19:54:23 +0530

    Implemented side scroll version 1.0.2

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const p1=document.querySelector('.main');
+console.log(`teh mainis ${p1}`)
+
+const flag=false;
+
+const np=document.querySelector('.next-page')
+const dp=document.querySelector('.loaded-page')
+
+np.appendChild(p1)
+
+const main = document.querySelector('.loading')
+console.log(main)
+
+main.addEventListener("animationend", (event) => {
+    console.log('the animation ended', event);
+    main.remove()
+    main.classList.remove('animate')
+    dp.appendChild(main)
+})
